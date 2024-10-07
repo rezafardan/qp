b0914f7871ad04d11494995884b32de8337d3666
Reza Fardan <rezafardaaan@gmail.com>
2024-10-07 11:32:30 +0700

    update homepage url

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./components/i18n.js";
-
 import Galaxy from "./components/hero/Galaxy.jsx";
 
 createRoot(document.getElementById("root")).render(
